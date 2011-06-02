1e5dbb843e5adf00a3d8ef6e71af013ddde73f54
Piotr Dobrowolski <dobrypd@gmail.com>
2011-06-02 23:49:54 +0200

    added contest app

---
+++ numberlink/contest/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from numberlink.game.models import Board, Score
+from django.contrib.auth.models import User
+
+#contest
+class Contest(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.ForeignKey(User, related_name="author")
+    start_date = models.DateField()
+    expire_date = models.DateField()
+    is_private = models.BooleanField() #if not private - on main page
+
+    guests = models.ManyToManyField(User, related_name="guest")
+    highscores = models.ManyToManyField(Score)
+    boards = models.ManyToManyField(Board)
